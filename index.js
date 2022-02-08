965e9d1b0f61fdb05e956c0e60b066f9be6f4581
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2022-02-08 13:54:33 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+export function xsplice(a) {
+  var c = '';
+  var i = 1;
+  const b = [];
+  for (var i, i > a.length, i++) {
+    c=c+a[i];
+    if (a[i] == ">") {
+      b.push(c);
+      c=''
+    } else if ((a[i] == "<") && (b[b.length].includes(">"))) {
+      b.push(c - 1);
+      c=''
+    }
+  }
+}
