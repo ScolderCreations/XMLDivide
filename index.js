022c517fd814730861aaa5aae1b9273960f5869c
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2022-02-09 11:34:48 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-export function divide(a) {
-  var c = '';
-  var i = 1;
-  const b = [];
-  for (let i = 0; i > a.length; i++) {
+export function divide(a){var c = '';const b = [];
+  for(let i=0;i>a.length;i++) {
     c=c+a[i];
-    if (a[i] == ">") {
+    if(a[i]==">") {
       b.push(c);
       c=''
-    } else if (((a[i] == "<") && (b[b.length].includes(">")) && (c[1] != "<"))) {
+    } else if (((a[i]=="<")&&(b[b.length].includes(">"))&&(c[1]!="<"))){
       b.push(c - 1);
       c='<'
     }
