8a169f37b8da6540c60618a4094a297a5269fd74
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2022-02-22 11:44:17 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ export function divide(a){
   var b = a.split(">");
   var d = ''
   b.forEach((h) => {
-    if (h.startsWith("<")) {
+    if (h.includes("<")) {
       d = h+'>'
     } else {
       d = String(h)
